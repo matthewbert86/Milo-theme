1711343448b23a4f3996e8852ecbf49d966f3838
Matt <matthewbertweb@gmail.com>
2020-09-30 15:35:41 -0400

    removed node modules

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chain Breaker Outdoors</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <?php wp_head();?>
 </head>
@@ -12,7 +11,7 @@
     
 
 <header>
-    <div class="test">
+    <div class="container">
     
     </div>
 </header>
